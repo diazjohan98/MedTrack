3034a77bf0416655d62cf44c18351a05bfd6c4bf
jhosebro <jhosebro2108@gmail.com>
2023-09-17 14:56:16 -0500

    Emergency terminado

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Emergency } from './modules/MedTrack/emergencia/Emergency'
 // import { Login } from './modules/MedTrack/login/Login.jsx'
-import { Registrarse } from './modules/MedTrack/registro/Registrarse'
+//import { Registrarse } from './modules/MedTrack/registro/Registrarse'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Login /> */}
-    <Registrarse/>
+    <Emergency />
   </React.StrictMode>,
 )
--- src/modules/MedTrack/emergencia/Emergency.jsx
+++ src/modules/MedTrack/emergencia/Emergency.jsx
@@ -1,34 +1,46 @@
 import Perfil from "../../../assets/Perfil.png";
 import IconoEnfermero from "../../../assets/IconoEnfermero.png";
 import ConfigIcon from "../../../assets/ConfigIcon.png";
-import IconoLlamar from "../../../assets/IconoLlamar.png"
+import IconoLlamar from "../../../assets/IconoLlamar.png";
 
 export const Emergency = () => {
   return (
-    <div className="container">
+    <div className="container pt-5">
       <div className="row">
-        <img src={Perfil} className="img-fluid col-3"></img>
-        <img src={IconoEnfermero} className="img-fluid col-3"></img>
-        <button type="button" className="btn col-3">
-          <img src={ConfigIcon} className="img-fluid"></img>
-        </button>
+        <div className="col-4">
+          <img src={Perfil} className="img-fluid pt-4" ></img>
+        </div>
+        <div className="col-4">
+          <img src={IconoEnfermero} className="img-fluid"></img>
+        </div>
+        <div className="col-4">
+          <button type="button" className="btn">
+            <img src={ConfigIcon} className="img-fluid pt-2"></img>
+          </button>
+        </div>
       </div>
-      <div>
+      <div className="p-4 pt-5 ps-2">
         <button type="button" className="btn">
-        <p className="fs-1">Información personal</p>
+          <p className="fs-1">Información personal</p>
         </button>
       </div>
-      <div>
+      <div className="p-4 ps-2">
         <button type="button" className="btn">
           <p className="fs-1">Medicamentos</p>
         </button>
       </div>
-      <div>
+      <div className="p-4">
         <p className="fs-1">Contacto de Emergencia</p>
+      </div>
+      <div className="mx-auto" style={{width: 100}}>
+        <button type="button" className="btn">
+          <img src={IconoLlamar} className="img-fluid"></img>
+        </button>
+        <p className="fs-1">Llamar</p>
       </div>
       <div>
         <button type="button" className="btn">
-        <img src={IconoLlamar} className="img-fluid"></img>
+          <p className="fs-1" style={{color: "#5BD654"}}>Cerrar Sesion</p>
         </button>
       </div>
     </div>
