7fda2fa86e392b85bf03a15f6a98e25f1d6ba219
JohanD <vasquez.johan@correounivalle.edu.co>
2023-09-18 11:26:47 -0500

    Creacion de la medicamentos

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { Login } from './modules/MedTrack/login/Login.jsx'
-import { Registrarse } from './modules/MedTrack/registro/Registrarse'
+// import { Registrarse } from './modules/MedTrack/registro/Registrarse'
+import { Medicamento } from './modules/MedTrack/medicamento/Medicamento'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Login /> */}
-    <Registrarse/>
+    {/* <Registrarse/>
+   */}
+    <Medicamento />
   </React.StrictMode>,
 )
