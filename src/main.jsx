a7efebd824b7917d29ad75caf789d72def9a524b
JohanD <vasquez.johan@correounivalle.edu.co>
2023-09-18 16:18:44 -0500

    aceptaion del pull en el main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Login } from './modules/MedTrack/login/Login.jsx'
-// import { MedTrackRoutes } from './routes/MedTrackRoutes.jsx'
 import { MedTrack } from './MedTrack.jsx'
-// import { Registrarse } from './modules/MedTrack/registro/Registrarse'
-// import { Medicamento } from './modules/MedTrack/medicamento/Medicamento'
-// import { MedTrackRoutes } from './routes/MedTrackRoutes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
