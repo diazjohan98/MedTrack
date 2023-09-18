10315be888ff69e31cdb8d7685c97c43e0ca3951
jhosebro <jhosebro2108@gmail.com>
2023-09-18 08:47:21 -0500

    Agrego pantalla de perfil

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Emergency } from './modules/MedTrack/emergencia/Emergency'
-// import { Login } from './modules/MedTrack/login/Login.jsx'
+import { Perfil } from './modules/MedTrack/perfil/Perfil'
+//import { Emergency } from './modules/MedTrack/emergencia/Emergency'
+//import { Login } from './modules/MedTrack/login/Login.jsx'
 //import { Registrarse } from './modules/MedTrack/registro/Registrarse'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Emergency />
+    <Perfil></Perfil>
   </React.StrictMode>,
 )
---
+++ src/modules/MedTrack/perfil/Perfil.jsx
@@ -0,0 +1,55 @@
+import Perfil1 from "../../../assets/Perfil.png";
+import IconoEnfermero from "../../../assets/IconoEnfermero.png";
+import ConfigIcon from "../../../assets/ConfigIcon.png";
+
+export const Perfil = () => {
+  return (
+    <div className="container pt-5">
+      <div className="row">
+        <div className="col-4">
+          <img src={Perfil1} className="img-fluid pt-4"></img>
+        </div>
+        <div className="col-4">
+          <img src={IconoEnfermero} className="img-fluid"></img>
+        </div>
+        <div className="col-4">
+          <button type="button" className="btn">
+            <img src={ConfigIcon} className="img-fluid pt-2"></img>
+          </button>
+        </div>
+      </div>
+      <div className="pt-5 ps-2">
+        <button type="button" className="btn">
+          <p className="fs-1">Nombre</p>
+        </button>
+      </div>
+      <div className="ps-2">
+        <button type="button" className="btn">
+          <p className="fs-1">Fecha de nacimiento</p>
+        </button>
+      </div>
+      <div className="ps-2">
+        <button type="button" className="btn">
+          <p className="fs-1">Peso</p>
+        </button>
+      </div>
+      <div className="ps-2">
+        <button type="button" className="btn">
+          <p className="fs-1">Estatura</p>
+        </button>
+      </div>
+      <div className="ps-2">
+        <button type="button" className="btn">
+          <p className="fs-1">Contacto de emergencia</p>
+        </button>
+      </div>
+      <div className="pt-4">
+        <button type="button" className="btn">
+          <p className="fs-1" style={{ color: "#5BD654" }}>
+            Cerrar Sesion
+          </p>
+        </button>
+      </div>
+    </div>
+  );
+};
