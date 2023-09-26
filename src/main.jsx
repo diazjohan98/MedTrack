5f542b857c59f14c2ed4c71990068ae52a176b39
jhosebro <jhosebro2108@gmail.com>
2023-09-26 09:43:27 -0500

    Fix Perfil y Graficos Funcion modo noche

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<<<< Temporary merge branch 1
-import { Graficos } from './modules/MedTrack/graficos/Graficos'
-//import { Perfil } from './modules/MedTrack/perfil/Perfil'
-=========
-
-// import { Login } from './modules/MedTrack/login/Login.jsx'
-// import { Registrarse } from './modules/MedTrack/registro/Registrarse'
-
-import { Perfil } from './modules/MedTrack/perfil/Perfil'
->>>>>>>>> Temporary merge branch 2
-//import { Emergency } from './modules/MedTrack/emergencia/Emergency'
-//import { Login } from './modules/MedTrack/login/Login.jsx'
-//import { Registrarse } from './modules/MedTrack/registro/Registrarse'
-
+import { MedTrack } from './MedTrack.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<<<< Temporary merge branch 1
-    <Graficos></Graficos>
-=========
-
-    {/* <Login /> */}
-    {/* <Registrarse/>
-   */}
-    <Perfil />
-
->>>>>>>>> Temporary merge branch 2
+    <MedTrack />
   </React.StrictMode>,
 )
 
--- src/modules/MedTrack/perfil/Perfil.jsx
+++ src/modules/MedTrack/perfil/Perfil.jsx
@@ -1,69 +1,70 @@
-import PerfilIcono from "../../../assets/Perfil.png";
+import vectorPNG from "../../../assets/img/Vector.png";
 import IconoEnfermero from "../../../assets/IconoEnfermero.png";
 // import ConfigIcon from "../../../assets/ConfigIcon.png";
 import { Sidebar } from "../../../components/sidebar/Sidebar";
 import { useNavigate } from "react-router-dom";
-import './css/perfilStyle.css'
+import "./css/perfilStyle.css";
 
 export const Perfil = () => {
-  const navigate = useNavigate()
-    const handleSubmitEmergencia = () => {
-        navigate('/emergency')
-
-    }
-    const handleSubmitHistory = () => {
-        navigate('/historia')
-
-    }
+  const navigate = useNavigate();
+  const handleSubmitEmergencia = () => {
+    navigate("/emergency");
+  };
+  const handleSubmitHistory = () => {
+    navigate("/historia");
+  };
   return (
     <div>
-      <Sidebar/>
-    <div className="container pt-5 mx-5">
-      <div className="row">
-        <div className="col-4">
-          <img src={PerfilIcono} className="img-fluid pt-4 mx-2"></img>
+      <Sidebar />
+      <div className="container pt-4 mx-5">
+        <div className="row">
+          <div className="titleContainer mx-3 col-3 ">
+              <img src={vectorPNG} alt="" className="mx-1 pb-4" />
+              <p className="titleMedicamentos">Perfil</p>
+          </div>
+          <div className="col-5">
+            <img src={IconoEnfermero} className="img-fluid pt-4"></img>
+          </div>
         </div>
-        <div className="col-4">
-          <img src={IconoEnfermero} className="img-fluid"></img>
-        </div>
-        {/* <div className="col-4">
+        <div className="pt-5 ps-2">
           <button type="button" className="btn">
-            <img src={ConfigIcon} className="img-fluid pt-2"></img>
+            <p className="fs-1">Nombre</p>
           </button>
-        </div> */}
+        </div>
+        <div className="ps-2">
+          <button type="button" className="btn">
+            <p className="fs-1">Fecha de nacimiento</p>
+          </button>
+        </div>
+        <div className="ps-2">
+          <button type="button" className="btn">
+            <p className="fs-1">Peso</p>
+          </button>
+        </div>
+        <div className="ps-2">
+          <button type="button" className="btn">
+            <p className="fs-1">Estatura</p>
+          </button>
+        </div>
+        <div className="ps-2">
+          <button
+            type="button mt-3"
+            className="btn btn-outline-success"
+            onClick={handleSubmitEmergencia}
+          >
+            <p className="fs-1 btnPerfil">Contacto de emergencia</p>
+          </button>
+        </div>
+        <div className="ps-2 mt-3">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            onClick={handleSubmitHistory}
+          >
+            <p className="fs-1 btnPerfil">Historia clinica</p>
+          </button>
+        </div>
       </div>
-      <div className="pt-5 ps-2">
-        <button type="button" className="btn">
-          <p className="fs-1">Nombre</p>
-        </button>
-      </div>
-      <div className="ps-2">
-        <button type="button" className="btn">
-          <p className="fs-1">Fecha de nacimiento</p>
-        </button>
-      </div>
-      <div className="ps-2">
-        <button type="button" className="btn">
-          <p className="fs-1">Peso</p>
-        </button>
-      </div>
-      <div className="ps-2">
-        <button type="button" className="btn">
-          <p className="fs-1">Estatura</p>
-        </button>
-      </div>
-      <div className="ps-2">
-        <button type="button mt-3" className="btn btn-outline-success" onClick={handleSubmitEmergencia}>
-          <p className="fs-1 btnPerfil">Contacto de emergencia</p>
-        </button>
-      </div>
-      <div className="ps-2 mt-3">
-        <button type="button" className="btn btn-outline-success" onClick={handleSubmitHistory}>
-          <p className="fs-1 btnPerfil">Historia clinica</p>
-        </button>
-      </div>
-      
-    </div>
     </div>
   );
 };
